1d3ef97cfd3952899c3aae941aa99526c98756cd
KacperSynator <62207289+KacperSynator@users.noreply.github.com>
2022-03-22 22:23:03 +0100

    WPF demo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace YouTubeTracker
         {
             InitializeComponent();
         }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"aaaaahh {firstNameText.Text}");
+        }
     }
 }
