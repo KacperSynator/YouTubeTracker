8efcd84c5b2945d5de0b8e19eb4473cae35dd244
KacperSynator <62207289+KacperSynator@users.noreply.github.com>
2022-04-16 13:07:42 +0200

    Add YoutubeTracker, listbox with images and tiltles,

---
+++ ListBoxItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace YouTubeTracker
+{
+    public class ListBoxItem
+    {
+        public string text { get; set; }
+        public Uri image { get; set; }
+        public ListBoxItem(string _text, string image_url)
+        {
+            text = _text;
+            image = new Uri(image_url);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,29 +26,32 @@ namespace YouTubeTracker
             InitializeComponent();
         }
 
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (YoutubeTracker.Search(videoPhrase.Text, 10))
+            {
+                foreach (var video in YoutubeTracker.GetLoadedVideos())
+                {
+                    videoListBox.Items.Add(new ListBoxItem(video.title, video.thumbnail_url));
+                }
+            }
+        }
+
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
-            var search_result = new YoutubeSearch().Search(videoPhrase.Text, 10);
+            var idx = videoListBox.SelectedIndex;
+            if (idx == -1) return;
 
-            if (search_result == null) return;
-
-            textList.Text = String.Format("Videos:\n{0}\n\nChannels:\n{1}\n\nPlaylists:\n{2}\n\n",
-                                            string.Join("\n", search_result.videos.Select(x => x.title)),
-                                            string.Join("\n", search_result.channels.Select(x => x.title)),
-                                            string.Join("\n", search_result.playlists.Select(x => x.title))
-                                          );
-
-            var id = search_result.videos[0].id;
-            var video_result = new YoutubeVideo().VideoList(new List<string> {id});
-            if (video_result == null) return;
-            //textList.Text += String.Format("Video info:\n{0}\n\n", string.Join("\n", video_result[0]));
+            var loaded_videos = YoutubeTracker.GetLoadedVideos();
+            var embed_html = loaded_videos[videoListBox.SelectedIndex].embed_html;
+            var id = loaded_videos[videoListBox.SelectedIndex].id;
 
             string html = "<html><head>" +
                 "<meta content='chrome=1,IE=Edge' http-equiv='X-UA-Compatible'/>" +
-                video_result.videos_data[0].embed_html.Insert(video_result.videos_data[0].embed_html.IndexOf("//www"), "https:") +
+                embed_html.Insert(embed_html.IndexOf("//www"), "https:") +
                 "<body style=\"background-color:black;\"></body>" +
                 "</head></html>";
-            this.videoWeb.NavigateToString(string.Format(html, id));
+            videoWeb.NavigateToString(string.Format(html, id));
         }
 
         private void videoPhrase_TextChanged(object sender, TextChangedEventArgs e)
